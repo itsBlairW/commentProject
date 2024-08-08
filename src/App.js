854104e57e6f2bfb0d57aed0bd5d2ac7cf10af3b
itsBlair <71138822+Jingyiii-W@users.noreply.github.com>
2024-08-08 00:01:28 -0400

    promot project by packing comment list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 //import { handleClientScriptLoad } from 'next/script'
 import './App.scss'
 import avatar from './images/bozai.png'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import _ from 'lodash'
 import classNames from 'classnames'
 import {v4 as uuidV4} from 'uuid'
 import dayjs from 'dayjs'
+import axios from 'axios'
 
 /**
  * 评论列表的渲染和操作
@@ -14,46 +15,7 @@ import dayjs from 'dayjs'
  * 2. 删除评论
  */
 
-// 评论列表数据
-const defaultList = [
-  {
-    // 评论id
-    rpid: 3,
-    // 用户信息
-    user: {
-      uid: '13258165',
-      avatar: '',
-      uname: '周杰伦',
-    },
-    // 评论内容
-    content: '哎哟，不错哦',
-    // 评论时间
-    ctime: '10-18 08:15',
-    like: 126,
-  },
-  {
-    rpid: 2,
-    user: {
-      uid: '36080105',
-      avatar: '',
-      uname: '许嵩',
-    },
-    content: '我寻你千百度 日出到迟暮',
-    ctime: '11-13 11:29',
-    like: 88,
-  },
-  {
-    rpid: 1,
-    user: {
-      uid: '30009257',
-      avatar,
-      uname: 'Blair',
-    },
-    content: 'I am blair',
-    ctime: '10-19 09:00',
-    like: 66,
-  },
-]
+
 // 当前登录用户信息
 const user = {
   // 用户id
@@ -75,16 +37,81 @@ const user = {
 
 // 导航 Tab 数组
 const tabs = [
-  { type: 'hot', text: '最热' },
-  { type: 'time', text: '最新' },
+  { type: 'hot', text: 'Hottest' },
+  { type: 'time', text: 'Recent' },
 ]
+
+//封装请求数据的Hook
+function useGetList(){
+  //获取接口数据渲染
+  const [commentList, setCommentList] = useState([])
+
+  useEffect(() => {
+    //Request Data
+    async function getList() {
+      //axios request data
+      const res = await axios.get('http://localhost:3004/list')
+      setCommentList(res.data)
+    }
+    getList()
+  }, [])
+
+  return{
+    commentList,
+    setCommentList
+  }
+
+}
+
+//封装Item组件
+function Item({item, onDel}){
+  return(
+    <div className="reply-item">
+            {/* 头像 */}
+            <div className="root-reply-avatar">
+              <div className="bili-avatar">
+                <img
+                  className="bili-avatar-img"
+                  alt=""
+                  src={item.user.avatar}
+                />
+              </div>
+            </div>
+
+            <div className="content-wrap">
+              {/* 用户名 */}
+              <div className="user-info">
+                <div className="user-name">{item.user.uname}</div>
+              </div>
+              {/* 评论内容 */}
+              <div className="root-reply">
+                <span className="reply-content">{item.content}</span>
+                <div className="reply-info">
+                  {/* 评论时间 */}
+                  <span className="reply-time">{item.ctime}</span>
+                  {/* 评论数量 */}
+                  <span className="reply-time">Like:{item.like}</span>
+                  {/*条件：user.id == item.user.id */}
+                  {user.uid === item.user.uid &&
+                  <span className="delete-btn" onClick={() => onDel(item.rpid)}>
+                    Delete
+                  </span>}
+                </div>
+              </div>
+            </div>
+          </div>
+  )
+}
 
 
 const App = () => {
   //渲染评论列表
   //1. 使用useState维护list
-  const [commentList, setCommentList] = useState(_.orderBy(defaultList,'like', 'desc'))
+  // const [commentList, setCommentList] = useState(_.orderBy(defaultList,'like', 'desc'))
 
+  //调用函数解构复制
+  const {commentList, setCommentList} = useGetList()
+  
   //删除
   const handleDelete = (id) =>{
     console.log(id)
@@ -142,7 +169,7 @@ const App = () => {
       <div className="reply-navigation">
         <ul className="nav-bar">
           <li className="nav-title">
-            <span className="nav-title-text">评论</span>
+            <span className="nav-title-text">Comment</span>
             {/* 评论数量 */}
             <span className="total-reply">{10}</span>
           </li>
@@ -172,56 +199,21 @@ const App = () => {
             {/* 评论框 */}
             <textarea
               className="reply-box-textarea"
-              placeholder="发一条友善的评论"
+              placeholder="Say something friendly~"
               ref={inputRef}
               value={content}
               onChange={(e) => setContent(e.target.value)}
             />
             {/* 发布按钮 */}
             <div className="reply-box-send">
-              <div className="send-text" onClick={handlePublish}>发布</div>
+              <div className="send-text" onClick={handlePublish}>Publish</div>
             </div>
           </div>
         </div>
         {/* 评论列表 */}
         <div className="reply-list">
           {/* 评论项 */}
-          {commentList.map(item=> (
-            <div key={item.rpid} className="reply-item">
-            {/* 头像 */}
-            <div className="root-reply-avatar">
-              <div className="bili-avatar">
-                <img
-                  className="bili-avatar-img"
-                  alt=""
-                  src={item.user.avatar}
-                />
-              </div>
-            </div>
-
-            <div className="content-wrap">
-              {/* 用户名 */}
-              <div className="user-info">
-                <div className="user-name">{item.user.uname}</div>
-              </div>
-              {/* 评论内容 */}
-              <div className="root-reply">
-                <span className="reply-content">{item.content}</span>
-                <div className="reply-info">
-                  {/* 评论时间 */}
-                  <span className="reply-time">{item.ctime}</span>
-                  {/* 评论数量 */}
-                  <span className="reply-time">点赞数:{item.like}</span>
-                  {/*条件：user.id == item.user.id */}
-                  {user.uid === item.user.uid &&
-                  <span className="delete-btn" onClick={()=>handleDelete(item.rpid)}>
-                    删除
-                  </span>}
-                </div>
-              </div>
-            </div>
-          </div>
-          ))}
+          {commentList.map(item=> <Item key = {item.id} item={item} onDel ={handleDelete}/>)}
         </div>
       </div>
     </div>
